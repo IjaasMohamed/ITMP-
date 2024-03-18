2c822871c59dbcd49180ccfffa8a64b5659e19e5
Shukri Husaindeen <99581533+ShukriHusaindeen@users.noreply.github.com>
2024-03-18 19:45:58 +0530

    initial

---
+++ connect-wave/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDX0Dlk_AwhYGglcLgvWWJEKSeE1WrsE0",
+  authDomain: "connect-wave-fd57f.firebaseapp.com",
+  projectId: "connect-wave-fd57f",
+  storageBucket: "connect-wave-fd57f.appspot.com",
+  messagingSenderId: "949482228905",
+  appId: "1:949482228905:web:cfdf50b45b7c35d101edb8"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
